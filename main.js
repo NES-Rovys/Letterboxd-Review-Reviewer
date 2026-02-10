5d3d36cbe096f930cf4cfeda7beb990c361fc47f
NES-Rovys <nes.rovys@gmail.com>
2026-02-10 14:17:18 +1100

    Finished(?) movie search function

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const BASE_URL = "https://api.themoviedb.org/3/";
 const API_KEY = "api_key=9b62c3eb4a6bc8acd4e26602f16fa744";
 let SEARCH_URL = BASE_URL + "search/movie?" + API_KEY + "&sort_by=popularity.desc&query=";
-
-console.log("\/");
+// Get shows as well
 
 function getMovies(my_api) {
   return fetch(my_api, {
@@ -17,36 +16,34 @@ function getMovies(my_api) {
 
 function renderMovies(res) {
   let ul = document.getElementById('results');
+  ul.classList.add("show");
   ul.innerHTML = '';
   res.results.forEach(result => {
     let li = document.createElement('li');
-    li.appendChild(document.createTextNode(result.title));
+    li.setAttribute('onClick', 'setMovie(this)');
+    li.textContent = result.title;
     ul.appendChild(li);
-    let img = document.createElement('img');
+    /*let img = document.createElement('img');
     img.src = "https://image.tmdb.org/t/p/w1280" + result.poster_path;
     img.width = "100";
     img.height = "100";
-    ul.appendChild(img);
+    ul.appendChild(img);*/
   });
 }
 
-let f = document.getElementById('search_movie');
-f.addEventListener('click', () => {
+let f = document.getElementById('search_input');
+f.addEventListener('input', () => {
   let user_input = search_input.value;
   if (user_input && user_input.trim() != '') {
     let query = SEARCH_URL + user_input;
     getMovies(query).then(renderMovies)
   }
+  if (f.textContent = '') {
+    document.getElementById('results').classList.remove("show");
+  }
 });
 
-function searchMovie() {
-    let f = document.getElementById('search_movie');
-    f.addEventListener('submit', () => {
-        let user_input = search_input.value;
-        if (user_input && user_input.trim() != '') {
-            let query = SEARCH_URL + user_input;
-            console.log();
-            getMovies(query);
-        }
-    });
+function setMovie(movie) {
+  document.getElementById('search_input').value = movie.textContent;
+  document.getElementById('results').classList.remove("show");
 }
