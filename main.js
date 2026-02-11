3eabff82abbb9757865b7bd34ee9a7cc192052e8
NES-Rovys <nes.rovys@gmail.com>
2026-02-11 17:25:37 +1100

    Added star logic

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// SEARCH STUFF
+// LOCATED HERE
 const BASE_URL = "https://api.themoviedb.org/3/";
 const API_KEY = "api_key=9b62c3eb4a6bc8acd4e26602f16fa744";
 let SEARCH_URL = BASE_URL + "search/movie?" + API_KEY + "&sort_by=popularity.desc&query=";
@@ -31,14 +33,14 @@ function renderMovies(res) {
   });
 }
 
-let f = document.getElementById('search_input');
-f.addEventListener('input', () => {
+let fSearch = document.getElementById('search_input');
+fSearch.addEventListener('input', () => {
   let user_input = search_input.value;
   if (user_input && user_input.trim() != '') {
     let query = SEARCH_URL + user_input;
     getMovies(query).then(renderMovies)
   }
-  if (f.textContent = '') {
+  if (fSearch.textContent = '') {
     document.getElementById('results').classList.remove("show");
   }
 });
@@ -47,3 +49,64 @@ function setMovie(movie) {
   document.getElementById('search_input').value = movie.textContent;
   document.getElementById('results').classList.remove("show");
 }
+
+// STAR STUFF
+// OVER HERE
+var stars = 0;
+
+let fStar = document.getElementById('starContainer');
+fStar.addEventListener('mousemove', (event) => {
+  for (let i = 1; i < 6; i++) {
+    document.getElementById('temp').classList.remove('st' + i);
+  }
+  document.getElementById('temp').classList.add('show');
+  for (let i = 0; i < 10; i++) {
+    if ((event.clientX - fStar.clientLeft) / fStar.clientWidth >= i / 10) {
+      if (i % 2 == 0) {
+        document.getElementById((i / 2) + 1).classList.add('half');
+        document.getElementById('temp').classList.add('st' + ((i / 2) + 1));
+      } else {
+        document.getElementById((i + 1) / 2).classList.add('full');
+        document.getElementById((i + 1) / 2).classList.remove('half');
+        document.getElementById('temp').classList.remove('st' + ((i + 1) / 2));
+      }
+    } else {
+      if (i % 2 == 0) {
+        document.getElementById((i / 2) + 1).classList.remove('half');
+      } else {
+        document.getElementById((i + 1) / 2).classList.remove('full');
+      }
+    }
+  }
+});
+
+fStar.addEventListener('mouseleave', () => {
+  console.log('leave');
+  for (let i = 1; i < 6; i++) {
+    if (Math.ceil(stars) >= i) {
+      if (stars < i && stars > i - 1) {
+        document.getElementById(i).classList.add('half');
+        document.getElementById('temp').classList.add('st' + i);
+      } else {
+        document.getElementById(i).classList.add('full');
+        document.getElementById(i).classList.remove('half');
+        document.getElementById('temp').classList.remove('st' + i);
+      }
+    } else {
+      document.getElementById(i).classList.remove('half');
+      document.getElementById(i).classList.remove('full');
+    }
+  }
+});
+console.log();
+fStar.addEventListener('mouseup', () => {
+  for (let i = 5; i > -1; i--) {
+    if (document.getElementById(i).classList.contains('full')) {
+      stars = i;
+      return;
+    } else if (document.getElementById(i).classList.contains('half')) {
+      stars = i - 0.5;
+      return;
+    }
+  }
+})
